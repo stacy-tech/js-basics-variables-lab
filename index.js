3f2feb9afe0a1fe074f3b741dcfad8621d01f457
stacy <stacydn23@gmail.com>
2021-05-14 00:11:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+// companyName is defined as a const 
+const companyName = 'Scuber';
+
+// mostProfitableNeighborhood defined using let and is = to Chelsea
+let mostProfitableNeighborhood = 'Chelsea'
+
+// companyCeo defined using let and is = to Susan Smith
+let companyCeo = 'Susan Smith'
